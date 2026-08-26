e786e30fa036901399d5c22c147ae4974d9009d0
ftbento <bhosein.dev@gmail.com>
2026-08-26 16:25:28 -0400

    cop3223c secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,12 @@
 let
   # username = "ssh-ed25519 PUBLIC_KEY";
   benjidev_radon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGESGigvG8ZWWQIv5S+Kg7ECkbwFxSoBIX29tNh/BmZu benjidev@radon";
+  benjidev_elitebook_wsl   = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB4kXVbiNiirKZkljHnD+t15Hd9iTocub0TbKU8s8m00 benjidev@Ben-Elitebook";
 in
 {
   # "github-ssh-key.age".publicKeys = [ USERNAME-HOSTNAME ];
-  "github-ftbento.age".publicKeys = [ benjidev_radon ];
+  "github-ftbento.age".publicKeys = [ benjidev_radon benjidev_elitebook_wsl ];
+  "cop3223c.age".publicKeys = [ benjidev_radon benjidev_elitebook_wsl];
 }
 
 # paste the private key in the editor
